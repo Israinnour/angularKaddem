ea637c8bbeb7341b73bdcc7d0c50671048253d72
Israinnour <israa.neji8@gmail.com>
2022-12-02 02:23:45 +0100

    gestion departement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { GestionDepartementModule } from './gestion-departement/gestion-departement.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GestionDepartementModule
   ],
   providers: [],
   bootstrap: [AppComponent]
